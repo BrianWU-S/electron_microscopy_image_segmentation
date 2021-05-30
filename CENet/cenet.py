39c24efffc44234e221d124f1aa915070611c89c
houshengyuan <445164577@qq.com>
2021-05-30 21:04:03 +0800

    update

--- CENet/cenet.py
+++ CENet/cenet.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python 
 # -*- coding:utf-8 -*-
+from torch import nn
+
+class UNet(nn.Module):
+    def __init__(self):
+        super().__init__()
 
 
-if __name__ == "main":
+if __name__ == "__main__":
     pass
---
+++ utils.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python 
+# -*- coding:utf-8 -*-
+import logging
+import time
+
+
+class ISBIdataset():
+   def __init__(self):
+      pass
